57060b1bbb51120b584a8f91dc84e12e47655dcd
Darcio <dpensky@gmail.com>
2024-01-17 14:08:31 -0300

    allow product update without category

--- src/main/java/com/dpensky/desafioanotaai/services/ProductService.java
+++ src/main/java/com/dpensky/desafioanotaai/services/ProductService.java
@@ -38,8 +38,10 @@ public class ProductService {
     public Product update(String id, ProductDTO productData) {
         Product product = this.productRepository.findById(id)
                 .orElseThrow(ProductNotFoundException::new);
-        this.categoryService.getById(productData.categoryId())
-                .ifPresent(product::setCategory);
+        if (productData.categoryId() != null) {
+            this.categoryService.getById(productData.categoryId())
+                    .ifPresent(product::setCategory);
+        }
         if (!productData.title().isEmpty())
             product.setTitle(productData.title());
         if (!productData.description().isEmpty())
